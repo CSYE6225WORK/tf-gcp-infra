4641c661894739c5f341cd050547aaa10e94be50
Jason Li <59989287+Kotoriiii@users.noreply.github.com>
2024-02-15 13:42:06 -0500

    update the file
    * add the file
    
    * update the file

--- module/vpc/variables.tf
+++ module/vpc/variables.tf
@@ -1,3 +1,8 @@
+variable "name" {
+  type = list(string)
+  default = [ "vpc-network1", "vpc-network2" ]
+}
+
 variable "web_ip_cidr_range" {
   type = string
   default = "10.0.0.0/24"
@@ -7,3 +12,14 @@ variable "db_ip_cidr_range" {
   type = string
   default = "10.0.1.0/24"
 }
+
+variable "dest_range" {
+  type = string
+  default = "0.0.0.0/0"
+}
+
+variable "next_hop_gateway" {
+  type = string
+  default = "default-internet-gateway"
+}
+
