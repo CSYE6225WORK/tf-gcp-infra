9d72158d63a60b0c39e66f128cf0c489e46b221c
JasonLiii <ljq19970216@gmail.com>
2024-02-15 13:38:19 -0500

    update the file

--- module/vpc/main.tf
+++ module/vpc/main.tf
@@ -1,25 +1,33 @@
-resource "google_compute_network" "vpc_network" {
-  name                            = "vpc-network"
+resource "google_compute_network" "vpc" {
+  count                           = length(var.name)
+  name                            = var.name[count.index]
+  description                     = "this is my csye6225 class vpc network region"
   auto_create_subnetworks         = false
   routing_mode                    = "REGIONAL"
   delete_default_routes_on_create = true
 }
 
 resource "google_compute_subnetwork" "webapp_subnetwork" {
-  name          = "webapp"
+  count         = length(var.name)
+  name          = "webapp-${count.index+1}"
+  description   = "this is subnet work with webapp"
   ip_cidr_range = var.web_ip_cidr_range
-  network       = google_compute_network.vpc_network.id
+  network       = google_compute_network.vpc[count.index].id
 }
 
 resource "google_compute_subnetwork" "db_subnetwork" {
-  name          = "db"
+  count         = length(var.name)
+  name          = "db-${count.index+1}"
+  description   = "this is subnet work with database"
   ip_cidr_range = var.db_ip_cidr_range
-  network       = google_compute_network.vpc_network.id
+  network       = google_compute_network.vpc[count.index].id
 }
 
 resource "google_compute_route" "webapp_route" {
-  name             = "webapp-route"
-  dest_range       = "0.0.0.0/0"
-  network          = google_compute_network.vpc_network.id
-  next_hop_gateway = "default-internet-gateway"
+  count            = length(var.name)
+  name             = "webapp-route-${count.index+1}"
+  description      = "this is route to internet gateway which used with webapp subnet to be public"
+  dest_range       = var.dest_range
+  network          = google_compute_network.vpc[count.index].id
+  next_hop_gateway = var.next_hop_gateway
 }
--- module/vpc/variables.tf
+++ module/vpc/variables.tf
@@ -1,3 +1,8 @@
+variable "name" {
+  type = list(string)
+  default = [ "vpc-network1", "vpc-network2" ]
+}
+
 variable "web_ip_cidr_range" {
   type = string
   default = "10.0.0.0/24"
@@ -6,4 +11,14 @@ variable "web_ip_cidr_range" {
 variable "db_ip_cidr_range" {
   type = string
   default = "10.0.1.0/24"
+}
+
+variable "dest_range" {
+  type = string
+  default = "0.0.0.0/0"
+}
+
+variable "next_hop_gateway" {
+  type = string
+  default = "default-internet-gateway"
 }
